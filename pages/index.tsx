9359c524a7cdb77997a3d7cbfd23e4b4b6e619e7
Adil Khan <54783965+Adilkhan510@users.noreply.github.com>
2023-02-24 02:04:52 -0800

    chore: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { useRouter } from 'next/router';
 import HomeLayout from '../components/layout/home';
 
 export default function Home() {
-  const router = useRouter();
-
-  return <HomeLayout></HomeLayout>;
+  return (
+    <HomeLayout>
+      <div>Home</div>
+    </HomeLayout>
+  );
 }
